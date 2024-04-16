46af603b2656b4388b5dcc1a6a890fc385accf0e
Albert0PB <a23pebeal@iesgrancapitan.org>
2024-04-16 20:00:00 +0200

    añade programa en Python con errores

---
+++ ejemplo.py
@@ -0,0 +1,14 @@
+"""
+Programa en Python como ejemplo para Práctica 5 de Git en Entornos de Desarrollo
+
+Author: Alberto Pérez Bernabeu
+"""
+from random import rand int
+
+while true:
+    n = randint(1, 10)
+    if n = 7:
+        print('Lucky number, Slevin!')
+        break
+    else:
+        print(Better luck next time!)
